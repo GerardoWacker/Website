3c560710c3184d106dbee18ad7d4a9a68845e09b
Gerardo Wacker <ge@rar.vg>
2021-07-08 17:29:03 -0300

    Added edmail disclaimer page to router

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import { Home } from './pages/home'
 import { AppError } from './pages/error'
 import { RouterProvider } from './router/router';
 import { Route } from './router/route';
+import { EmailDisclaimer } from './pages/email-disclaimer';
 export const WebRoutes = [
     {
         path: "/",
         component: <Home />
+    },
+    {
+        path: "/email-disclaimer",
+        component: <EmailDisclaimer />
     },
     {
         path: "", // Empty string is used for the error page
